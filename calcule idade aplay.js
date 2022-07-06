e0a81fdb1b6115bccf497d2b21e4d0334b29d28c
Jazz Manhães <107090813+Jazz-Manhaes@users.noreply.github.com>
2022-07-06 13:29:41 -0300

    Add files via upload

---
+++ calcule idade aplay.js
@@ -0,0 +1,23 @@
+function calculaIdade( anos ) {
+    return `Daqui a ${anos} anos, ${this.name} terá ${
+        this.idade + anos
+    } anos de idade.`
+}
+
+const pessoal = {
+    nome: 'Maria',
+    idade: 30,
+}
+
+const pessoa2 = {
+    nome: 'Carla',
+    idade: 26,
+}
+
+const animal = {
+    nome: 'Fiona',
+    idade: 5,
+    raca: 'Pug',
+}
+
+console.log(calculaIdade.apply(pessoa2, [30]));
